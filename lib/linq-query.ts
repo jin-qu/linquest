9870403cf79140e3cf298b121c9c1041aa29af7e
Umut Özel <umutozel@outlook.com>
2018-09-11 23:37:33 +0300

    add include support

--- lib/linq-query.ts
+++ lib/linq-query.ts
@@ -1,8 +1,16 @@
-import { QueryPart, Query, PartArgument, AjaxFuncs, AjaxOptions } from "jinqu";
+import { QueryPart, Query, PartArgument, AjaxFuncs, AjaxOptions, Func1 } from "jinqu";
 
 export class LinqQuery<T, TOptions extends AjaxOptions = AjaxOptions> extends Query<T> {
+
+    include(selector: Func1<T, any>, ...scopes: any[]): LinqQuery<T> {
+        return <LinqQuery<T>>this.create(QueryPart.create(LinqFuncs.include, [PartArgument.identifier(selector, scopes)]));
+    }
 
     withOptions(options: TOptions): LinqQuery<T> {
         return <LinqQuery<T>>this.create(QueryPart.create(AjaxFuncs.options, [PartArgument.literal(options)]));
     }
 }
+
+const LinqFuncs = {
+    include: 'include'
+};
